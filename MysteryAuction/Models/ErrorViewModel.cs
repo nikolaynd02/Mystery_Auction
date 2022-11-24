d1c3f3b78bde5db5e93528733ff3ce4383a11cd6
Nikolay Dobrev <nickndobrev@gmail.com>
2022-11-24 21:44:24 +0200

    Deleted old version and started new

--- MysteryAuction/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MysteryAuction.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
