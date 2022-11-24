7f532941f9fdd32e056bcb2c87c151b25e2e672b
Nikolay Dobrev <nickndobrev@gmail.com>
2022-11-24 21:46:20 +0200

    The project anew

---
+++ MysteryAuction/MysteryAuction/Data/Models/DataConstraints.cs
@@ -0,0 +1,10 @@
+﻿namespace MysteryAuction.Data.Models
+{
+    public class DataConstraints
+    {
+        public class UserConstraints
+        {
+            public const int MaxUsernameLength = 20;
+        }
+    }
+}
---
+++ MysteryAuction/MysteryAuction/Data/Models/MysteryAuctionUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace MysteryAuction.Data.Models
+{
+    public class MysteryAuctionUser : IdentityUser
+    {
+        [Required] 
+        [MaxLength(DataConstraints.UserConstraints.MaxUsernameLength)]
+        public string Username { get; set; } = null!;
+    }
+}
