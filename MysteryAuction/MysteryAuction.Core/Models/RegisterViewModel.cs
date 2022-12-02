72a29494820934dbd9a49b6591f234f7f26609ca
Nikolay Dobrev <nickndobrev@gmail.com>
2022-12-02 23:55:02 +0200

    added core layer

---
+++ MysteryAuction/MysteryAuction.Core/Models/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MysteryAuction.Core.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [StringLength(60, MinimumLength = 10)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        [Compare(nameof(ConfirmPassword))]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
