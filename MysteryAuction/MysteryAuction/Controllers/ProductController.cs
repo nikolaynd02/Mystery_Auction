fdb591df2a6f07d67cf07e7b8ecd1e8fe674a74c
Nikolay Dobrev <nickndobrev@gmail.com>
2022-12-08 01:30:54 +0200

    created views and updated controller for products

--- MysteryAuction/MysteryAuction/Controllers/ProductController.cs
+++ MysteryAuction/MysteryAuction/Controllers/ProductController.cs
@@ -1,21 +1,61 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MysteryAuction.Core.Contracts;
+using MysteryAuction.Core.Models.Product;
+using MysteryAuction.Core.Services;
 
 namespace MysteryAuction.Controllers
 {
     [Authorize]
     public class ProductController : Controller
     {
-        //[HttpGet]
-        //public IActionResult Add()
-        //{
-            
-        //}
+        private readonly IProductService productService;
 
-        //[HttpPost]
-        //public IActionResult Add()
-        //{
+        public ProductController(IProductService _productService)
+        {
+            this.productService = _productService;
+        }
 
-        //}
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var model = await productService.GetAllProductsAsync();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var model = new AddProductViewModel()
+            {
+                Categories = await productService.GetAllCategoriesAsync()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddProductViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                await productService.AddProductAsync(model);
+
+                return RedirectToAction(nameof(All));
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("","Problem with adding product!");
+
+                return View(model);
+            }
+        }
+
     }
 }
