4cf49b95e354923c3cd5bb6da8bfb592b6767148
Nikolay Dobrev <nickndobrev@gmail.com>
2022-12-08 01:29:16 +0200

    Added dataconstraint for product

--- MysteryAuction/MysteryAuction.Infrastructure/Data/Models/DataConstraints.cs
+++ MysteryAuction/MysteryAuction.Infrastructure/Data/Models/DataConstraints.cs
@@ -4,10 +4,14 @@
     {
         public class MysteryProductConstraints
         {
-            public const int MaxDescriptionLength = 3000;
+            public const int MaxDescriptionLength = 3_000;
             public const int MinDescriptionLength = 30;
+
             public const int MaxNameLength = 20;
             public const int MinNameLength = 3;
+
+            public const int MaxPrice = 999_999_999;
+            public const int MinPrice = 1;
         }
 
         public class CategoryConstraints
