621c41928e3bba61cbf4872b9cb239543c986463
Nikolay Dobrev <nickndobrev@gmail.com>
2022-12-10 00:35:34 +0200

    updated bid and product models

--- MysteryAuction/MysteryAuction.Core/Models/Bid/AddBidViewModel.cs
+++ MysteryAuction/MysteryAuction.Core/Models/Bid/AddBidViewModel.cs
@@ -1,19 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using MysteryAuction.Infrastructure.Data.Models;
 
 namespace MysteryAuction.Core.Models.Bid
 {
     public class AddBidViewModel
     {
-        [Required] 
+        [Required]
+        [ValidateNever]
         public string UserId { get; set; } = null!;
 
         [Required]
         public Guid ProductId { get; set; }
 
         [Required]
+        [Range(DataConstraints.MysteryProductConstraints.MinPrice, DataConstraints.MysteryProductConstraints.MaxPrice)]
         public decimal Price { get; set; }
-
-        public DateTime MadeAt { get; set; }
 
     }
 }
--- MysteryAuction/MysteryAuction.Core/Models/Product/ProductViewModel.cs
+++ MysteryAuction/MysteryAuction.Core/Models/Product/ProductViewModel.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace MysteryAuction.Core.Models.Product
 {
     public class ProductViewModel
     {
+        public string Id { get; set; } = null!;
+
         public string ProductName { get; set; } = null!;
 
         public string Description { get; set; } = null!;
