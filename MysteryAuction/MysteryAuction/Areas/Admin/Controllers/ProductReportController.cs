aaa39c3c11ad563c1df6cad4118151863901bb5f
Nikolay Dobrev <nickndobrev@gmail.com>
2022-12-12 02:52:49 +0200

    Admin report view added

---
+++ MysteryAuction/MysteryAuction/Areas/Admin/Controllers/ProductReportController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MysteryAuction.Core.Contracts;
+
+namespace MysteryAuction.Areas.Admin.Controllers
+{
+    public class ProductReportController : BaseController
+    {
+        private readonly IProductReportService productReportService;
+
+        public ProductReportController(IProductReportService _productReportService)
+        {
+            this.productReportService = _productReportService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var model = await productReportService.GetAllAsync();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Resolved(Guid id)
+        {
+            await productReportService.Resolved(id);
+
+            return RedirectToAction("All");
+        }
+    }
+}
